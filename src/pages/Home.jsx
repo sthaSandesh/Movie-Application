e6a092b22c90672467bed75a12c493f823eb3e71
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-24 18:41:38 +0545

    item addd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Hero from '../component/Hero'
+import Row from '../component/Row'
+import request from '../Request'
 
 const Home = () => {
   return (
-    <div>
+    <div >
         <Hero />
+        <Row title='Up Coming' fetchURL={request.requestUpcoming} /> 
+        <Row title='Popular' fetchURL={request.requestPopular} /> 
+        <Row title='Trending' fetchURL={request.requestTrending} /> 
+        <Row title='Top Rated' fetchURL={request.requestTopRated} /> 
+        <Row title='Horror' fetchURL={request.requestHorror} /> 
     </div>
   )
 }
