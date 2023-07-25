bba41538dc36c48113db62b925c96d6e9d3af509
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-25 17:09:13 +0545

    arow for lg device ad

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Movie from './Movie';
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 const Row = ({ title, fetchURL }) => {
     const [movies, setMovies] = useState([]);
@@ -18,11 +19,13 @@ const Row = ({ title, fetchURL }) => {
         <>
             <h2 className='font-bold md:text-xl p-4 text-white'>{title}</h2>
             <div className=' relative flex items-center'>
+                <MdChevronLeft className='bg-white  rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10' size={40} /> 
                 <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) => (
                         <Movie key={id} item={item} />
                     ))}
                 </div>
+                <MdChevronLeft className='bg-white  rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10' size={40} />
             </div>
         </>
     );
