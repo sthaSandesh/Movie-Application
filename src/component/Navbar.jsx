0d91cb496d0e057c962a82e8a9f2c1c38c8c452f
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-07-25 17:46:46 +0545

    nav compont formated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex item-center justify-between p-4 z-[100] absolute w-full '>
-        <h1 className='font-bold text-red-600 text-4xl cursor-pointer'>NEPFLEX</h1>
-        <div>
-            <button className='text-white  pr-4 '>Sign In</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white '>Sign Up</button>
-        </div>
+      <h1 className='font-bold text-red-600 text-4xl cursor-pointer'>NEPFLEX</h1>
+      <div>
+        <button className='text-white  pr-4 '>Sign In</button>
+        <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white '>Sign Up</button>
+      </div>
     </div>
   )
 }
